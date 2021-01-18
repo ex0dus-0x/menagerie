d8ccc87c3ae45981de44697cc5b432ec3340b09e
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2021-01-18 11:55:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Eat My Shorts
+
+Tiny cross-platform malware development library for anti-reversing evasion techniques
+
+## Introduction
+
+Developing a custom malware/payload for a red team engagement? Using Golang because you love
+the cross-platform + static compilation support?
+
+## Techniques Employed
---
+++ lib.go
@@ -0,0 +1,67 @@
+// Implements main functionality for anti-reversing routines. Will run detection
+// routine based on specific platform the adversarial sample is on.
+package ems
+
+import (
+    "runtime"
+    "github.com/ex0dus-0x/ems/windows"
+    //"github.com/ex0dus-0x/ems/linux"
+    //"github.com/ex0dus-0x/ems/macos"
+)
+
+/*=========================== ANTI-DEBUGGING ===========================*/
+
+// Cross-platform function used to detect if a debugger is currently hooked onto the
+// current executable.
+func AntiDebugging() bool {
+    if runtime.GOOS == "windows" {
+        return windows.WindowsDebugging()
+    }
+
+    /*
+    else if runtime.GOOS == "linux" {
+        return linux.LinuxDebugging()
+    } else if runtime.GOOS == "macos" {
+        return macos.MacDebugging()
+    }
+    */
+
+    return false
+}
+
+// Runs cross-platform anti-debug detection, and run a callback routine if debugging is detected.
+func AntiDebuggingCb(cb func()) {
+    if AntiDebugging() == true {
+        cb()
+    }
+}
+
+/*=========================== ANTI-SANDBOXING ===========================*/
+
+func AntiSandbox() bool {
+    /*
+    if runtime.GOOS == "windows" {
+        return WindowsSandbox()
+    } else if runtime.GOOS == "linux" {
+        return LinuxSandbox()
+    } else if runtime.GOOS == "macos" {
+        return MacSandbox()
+    }
+    */
+    return false
+}
+
+// Runs cross-platform anti-sandbox detection, and run a callback routine if debugging is detected.
+func AntiSandboxCb(cb func()) {
+    if AntiSandbox() == true {
+        cb()
+    }
+}
+
+
+/*=========================== ANTI-DISASSEMBLY ===========================*/
+
+// Cross-platform function used to inject confusing assembler payloads before sensitive
+// functionality to trick any linear or recursive disassembler.
+func AntiDisassembly() {
+}
