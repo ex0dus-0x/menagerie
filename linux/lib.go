fadfee4119ac0b72d7e14cefe66c961819c785df
ex0dus-0x <ex0dus@codemuch.tech>
2021-01-21 22:37:37 -0500

    Refactor

--- linux/lib.go
+++ linux/lib.go
@@ -1,13 +1,26 @@
 package linux
 
-import "syscall"
+import (
+    "syscall"
+    "os/signal"
+)
 
-// Simplest form of anti-debug on Linux using `ptrace`.
+// Simplest form of anti-debug on Linux using `ptrace`, called twice to ensure that the detection
+// works if any hooking is done on the `ptrace` call itself.
 func CheckPtrace() bool {
-    // check for debugger
-    _, _, res := syscall.RawSyscall(syscall.SYS_PTRACE, uintptr(syscall.PTRACE_TRACEME), 0, 0)
-    if res == 1 {
-        return true
+    // stores final state that gets changed
+    debugged := false
+
+    sigchan := make(chan os.Signal, 1)
+    signal.Notify(sigchan, syscall.SIGSEGV)
+
+
+    exitchan := make(chan int)
+    go func() {
+
     }
-    return false
+
+    // check for debugger - first call may be hooked
+    syscall.RawSyscall(syscall.SYS_PTRACE, uintptr(syscall.PT_DENY_ATTACH), 0, 0)
+    return debugged
 }
--- macos/lib.go
+++ macos/lib.go
@@ -1,1 +1,20 @@
 package macos
+
+import "syscall"
+
+// Simplest form of anti-debug on macOS using `ptrace` with `PT_DENY_ATTACH`, called twice to
+// ensure that the detection works if any hooking is done on the `ptrace` call itself
+func CheckPtrace() bool {
+    state := 0
+
+    // check for debugger - first called may be hooked
+    if _, _, res := syscall.RawSyscall(syscall.SYS_PTRACE, uintptr(syscall.PT_DENY_ATTACH), 0, 0); res == 1 {
+        state = 1
+
+        // check for debugger again - second call to double-check
+        if _, _, res := syscall.RawSyscall(syscall.SYS_PTRACE, uintptr(syscall.PT_DENY_ATTACH), 0, 0); res == 1 {
+            state = 2
+        }
+    }
+    return state == 2
+}
