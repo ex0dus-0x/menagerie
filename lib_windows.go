c41e783d5c367ee2e5bf775d6cf63f0ccf73f263
ex0dus-0x <ex0dus@codemuch.tech>
2021-02-05 14:49:49 -0500

    Clean up organization

--- lib_windows.go
+++ lib_windows.go
@@ -1,38 +1,60 @@
-// Implements main functionality for anti-reversing routines. Will run detection
-// routine based on specific platform the adversarial sample is on.
 package ems
 
-import "github.com/ex0dus-0x/ems/windows"
+// #cgo CFLAGS:
+// #include "windows/antidbg.h"
+// #include "windows/antivm.h"
+// #include "windows/antidisass.h"
+import "C"
+
 
 /*=========================== ANTI-DEBUGGING ===========================*/
 
-// Cross-platform function used to detect if a debugger is currently hooked onto the
-// current executable.
+// Main routine to call to execute all known debugger detection heuristics.
+//  - Process Environment Block
+//  - Breakpoint Detection
 func AntiDebugging() bool {
 
     // basic PEB fingerprinting check
-    if windows.CheckDebuggerPEB() == true{
+    if CheckDebuggerPEB() == true {
         return true
     }
 
     // checks to see if breakpoint has been placed
-    if windows.CheckBreakpoint() == true {
+    if CheckBreakpoint() == true {
         return true
     }
 
     return false
 }
 
-// Runs cross-platform anti-debug detection, and run a callback routine if debugging is detected.
+// Runs anti-debug detection, and run a callback routine if debugging is detected.
 func AntiDebuggingCb(cb func()) {
     if AntiDebugging() == true {
         cb()
     }
 }
 
+//// WRAPPERS ////
+
+func CheckDebuggerPEB() bool {
+    return bool(C.CheckDebuggerPEB())
+}
+
+func CheckBreakpoint() bool {
+    return bool(C.CheckBreakpoint())
+}
+
 /*=========================== ANTI-SANDBOXING ===========================*/
 
+// Main routine to call to execute all known sandbox/VM detection heuristics.
+//  - CPUID hypervisor check
 func AntiSandbox() bool {
+
+    // check with classic `cpuid` instruction
+    if CheckCPUIDHypervisor() == true {
+        return true
+    }
+
     return false
 }
 
@@ -43,10 +65,16 @@ func AntiSandboxCb(cb func()) {
     }
 }
 
+//// WRAPPERS ////
+
+func CheckCPUIDHypervisor() bool {
+    return bool(C.CheckCPUIDHypervisor())
+}
 
 /*=========================== ANTI-DISASSEMBLY ===========================*/
 
-// Cross-platform function used to inject confusing assembler payloads before sensitive
-// functionality to trick any linear or recursive disassembler.
+// Injects tricky inlined assembly at any point of execution to confuse disassembler
+// and emit confusing disassembly.
 func AntiDisassembly() {
+    C.AntiDisassembly()
 }
