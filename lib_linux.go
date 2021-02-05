c90dc9271619019672edbbb93db14d9843b9ac59
ex0dus-0x <17283637+ex0dus-0x@users.noreply.github.com>
2021-02-05 16:49:50 -0500

    Update with techniques

--- lib_linux.go
+++ lib_linux.go
@@ -1,6 +1,6 @@
 package ems
 
-// #cgo LDFLAGS: -Wl,--no-as-needed -ldl
+// #cgo LDFLAGS: -Wl,--no-as-needed -ldl -lc
 // #include "linux/antidbg.h"
 // #include "linux/antivm.h"
 import "C"
@@ -9,23 +9,31 @@ import "C"
 
 // Main routine to call to execute all known debugger detection heuristics.
 //  - Stealthy Ptrace
+//  - Breakpoint Handler Detection
+//  - Procfs Fingerprinting
 func AntiDebugging() bool {
-
-    // stealthy but still basic ptrace check
-    if CheckStealthyPtrace() == true {
-        return true
-    }
-
-    return false
+    return CheckStealthyPtrace() || CheckBreakpoint() || CheckProcessFingerprint()
 }
 
 //// WRAPPERS ////
 
-// Defines a basic antidebug implementation that dynamically loads ptrace
-// from libc to mitigate the detection of symbols in static analysis
+// Basic anti-debug that dynamically loads ptrace to mitigate the detection 
+// of symbols in static analysis.
 func CheckStealthyPtrace() bool {
     return bool(C.CheckStealthyPtrace())
 }
+
+// Detects the presence of a debug by attempting to evoke a debugging SIGTRAP handler 
+// when setting a breakpoint.
+func CheckBreakpoint() bool {
+    return bool(C.CheckBreakpoint())
+}
+
+// Parses procfs metadata to fingerprint a tracer process for the current executable.
+func CheckProcessFingerprint() bool {
+    return bool(C.CheckProcessFingerprint())
+}
+
 
 /*=========================== ANTI-SANDBOXING ===========================*/
 
